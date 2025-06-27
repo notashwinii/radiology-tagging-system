613a6e7bde4ddc71f09b3f9afe3a32d62f09621b
notashwinii <ashwinii.subedi@gmail.com>
2025-06-27 08:54:25 +0545

    refactor: change login to jwt

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,31 @@
 # fastapi
 from fastapi import FastAPI
+from fastapi.openapi.models import APIKey, APIKeyIn, SecuritySchemeType
+from fastapi.openapi.utils import get_openapi
 from app.core.modules import init_routers, make_middleware
 
+
+def custom_openapi(app):
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title=app.title,
+        version=app.version,
+        description=app.description,
+        routes=app.routes,
+    )
+    openapi_schema["components"]["securitySchemes"] = {
+        "BearerAuth": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT"
+        }
+    }
+    for path in openapi_schema["paths"].values():
+        for method in path.values():
+            method.setdefault("security", []).append({"BearerAuth": []})
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
 
 def create_app() -> FastAPI:
     app_ = FastAPI(
@@ -12,6 +36,7 @@ def create_app() -> FastAPI:
         middleware=make_middleware(),
     )
     init_routers(app_=app_)
+    app_.openapi = lambda: custom_openapi(app_)
     return app_
 
 
