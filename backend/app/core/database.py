5d1497860df611c1b43297eb1cbc94c45b9d32c0
notashwinii <ashwinii.subedi@gmail.com>
2025-06-28 17:22:53 +0545

    fix: update database url

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:sugat123@localhost:5432/rad"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1198@localhost:5432/rad"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
